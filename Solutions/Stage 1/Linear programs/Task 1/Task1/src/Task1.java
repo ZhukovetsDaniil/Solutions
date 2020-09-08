052abc8d3b645b5b567f137981ecc2a94df29ba7
Даниил Жуковец <zhukovets.daniil@gmail.com>
2020-09-08 20:54:11 +0300

    complited linear programs task 1

---
+++ Solutions/Stage 1/Linear programs/Task 1/Task1/src/Task1.java
@@ -0,0 +1,18 @@
+
+/*Task 1:
+ *Найдите значение функции z = ((a - 3) * b / 2) + c
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        //set values for variables
+        double a = 10;
+        double b = 15;
+        double c = 21;
+
+        //calculate value of function
+        double result = ((a - 3) * b / 2) + c;
+        //display result to standard output stream (console)
+        System.out.println(result);
+
+    }
+}
